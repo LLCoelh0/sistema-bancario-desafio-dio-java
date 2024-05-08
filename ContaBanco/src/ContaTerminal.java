b346f50dc734df58135df8cb1273fac82d115f3c
LLCoelh0 <leonardo-coelho1@hotmail.com>
2024-05-08 12:16:16 -0300

    Commit final

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,11 +4,6 @@ public class ContaTerminal {
 
     public static void main(String[] args) {
 
-        //int number = 0;
-        //String agency = "Null";
-        //String clientName = "Null";
-        //double balance = 0.0;
-
         Scanner scNumber = new Scanner(System.in);
         System.out.print("Enter with the account number:");
         int accountNumber = scNumber.nextInt();
@@ -16,16 +11,20 @@ public class ContaTerminal {
         Scanner scAgency = new Scanner(System.in);
         System.out.print("Enter with the agency number:");
         String agency = scAgency.nextLine();
-
+        
         Scanner scName = new Scanner(System.in);
         System.out.print("Enter with your name:");
         String name = scName.nextLine();
-
+        
         Scanner scBalance = new Scanner(System.in);
         System.out.print("Enter with the balance: ");
         double balance = scBalance.nextDouble();
+        
+        System.out.printf("Hello %s, thank you for open an account in our Bank, your agency number is %s, account %d and your balance is %.2f is already available. \n", name, agency, accountNumber, balance);
 
-        System.out.printf("Hello %s, thank you for open an account in our Bank, your agency number is %s, account %d and your balance is %2f is already available.", name, agency, accountNumber, balance);
-
+        scNumber.close();
+        scAgency.close();
+        scName.close();
+        scBalance.close();
     }
 }
